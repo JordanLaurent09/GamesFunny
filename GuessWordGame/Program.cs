65da4be9741895676fbc3a5f62e83cf0ba081b81
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-01 17:08:12 +0200

    GuessWordGame

--- GuessWordGame/Program.cs
+++ GuessWordGame/Program.cs
@@ -10,6 +10,33 @@ namespace GuessWordGame
     {
         static void Main(string[] args)
         {
+            // Запуск игры - создание объекта класса Game
+            Game game = new Game();
+
+            // Создание игрока
+            Player player = game.CreatePlayer();
+
+            // Получение игроком списка фильмов
+            List<string> wordList = player.PrepareWords();
+
+            // Выдача игроку случайного фильма для угадывания названия
+            game.secretWord = player.GetRandomWord(wordList);
+
+            // Преобразование строки с названием фильма в зашифрованный массив символов, отображаемый на экране
+            char[] secret = player.getCharArray(game.secretWord);
+
+            // Игрок пытается угадать название фильма и по итогу своих попыток получает статус
+            player.playerState = player.SeekingWord(secret);
+
+            // Создание объекта класса Log - для ведения журнала успехов игроков
+            Log log = new Log();
+
+            // Следующие действия заполняют журнал
+            List<string> fileContent = log.ReadFile();
+
+            log.PrepareInfo(player, fileContent);
+
+            log.WriteData(fileContent);
         }
     }
 }
