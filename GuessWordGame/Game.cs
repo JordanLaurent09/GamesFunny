98c502941d95c06cf72b091734179216b886594e
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-01 17:00:18 +0200

    Game class

---
+++ GuessWordGame/Game.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessWordGame
+{
+    /* Класс Game запускает игру,а именно - 
+     * создает игрока */
+    class Game
+    {
+        public string secretWord;
+
+
+        // Метод по созданию игрока
+
+        public Player CreatePlayer()
+        {
+            Player player = new Player();
+            Console.WriteLine("Назови себя: ");
+            player.name = Console.ReadLine();
+            return player;
+        }
+    }
+}
