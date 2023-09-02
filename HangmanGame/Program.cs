fcce24e4b82b1a10ef089e40ebeb4262c7b59941
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-02 17:58:47 +0200

    Hangman game

---
+++ HangmanGame/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HangmanGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int lettersNumber = 10;
+            char[] guessedLetters;
+            char[] word;
+            bool GameOver = false;
+
+            Console.WriteLine("Загадайте слово");
+
+            string secretWord = Console.ReadLine();
+
+            guessedLetters = new char[secretWord.Length];
+
+            // Заполнение игровой строки прочерками
+            for (int i = 0; i < guessedLetters.Length; i++)
+            {
+                guessedLetters[i] = '-';
+            }
+            word = secretWord.ToCharArray();
+
+            Console.WriteLine("Теперь отгадайте введенное слово: ");
+
+            while (!GameOver)
+            {
+                if (lettersNumber == 0) GameOver = true;
+
+                for (int i = 0; i < guessedLetters.Length; i++)
+                {
+                    Console.Write(guessedLetters[i] + "");
+                }
+                Console.WriteLine();
+
+                // Проверка, угадано ли все слово целиком
+                if (!guessedLetters.Contains('-'))
+                {
+                    Console.WriteLine("Вы победили!");
+                    break;
+                }
+
+                // Пользовательский ввод буквы
+                char letter = char.Parse(Console.ReadLine());
+
+                // Вывод на экран массива с прочерками с угаданными буквами
+                if (word.Contains(letter))
+                {
+                    for (int i = 0; i < word.Length; i++)
+                    {
+                        if (word[i] == letter)
+                        {
+                            guessedLetters[i] = letter;
+                        }
+                    }
+                }
+                else
+                {
+                    lettersNumber--;
+                }
+            }
+        }
+    }
+}
