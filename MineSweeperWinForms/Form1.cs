27338669f02a7ce3f08c5184f0a34bb4e0a81996
JordanLaurent09 <128598320+JordanLaurent09@users.noreply.github.com>
2023-09-21 18:34:40 +0200

    first steps

--- MineSweeperWinForms/Form1.cs
+++ MineSweeperWinForms/Form1.cs
@@ -34,17 +34,29 @@ namespace MineSweeperWinForms
             return field;
         }
 
+        // Create an array with numbers, where number 0 means mine, and other numbers are "safe spots"
+        private int[] AllFieldCells(int size)
+        {
+            int[] cells = new int[size];
+            Random random = new Random();
+            for(int i = 0; i <size; i++)
+            {
+                cells[i] = random.Next(0, 10);
+            }
+            return cells;
+        }
+
         // Method for auto-creation of buttons
 
         private void CreateButtonField(int size)
         {
             // Initial position of first button
             int initX = 200;
-            int initY = 200;
+            int initY = 50;
 
             // Parameters of buttons
-            int heightBtn = 20;
-            int widthBtn = 20;
+            int heightBtn = 50;
+            int widthBtn = 50;
 
             // Creation of buttons array
 
@@ -57,14 +69,36 @@ namespace MineSweeperWinForms
                     buttons[y, x] = new Button();
                     buttons[y, x].Font = new Font(Font.FontFamily, 16);
                     buttons[y, x].SetBounds(initX, initY, widthBtn, heightBtn);
+                    buttons[y, x].Click += new System.EventHandler(Button_Click);
                     Controls.Add(buttons[y, x]);
-                    initX += 20;
+                    initX += 50;
                 }
                 initX = 200;
-                initY += 20;
+                initY += 50;
             }
         }
 
+
+        private void Button_Click(object sender, System.EventArgs e)
+        {
+            int[] cells = AllFieldCells(100);
+            int count = new Random().Next(0, cells.Length);
+           
+            
+                Button button = (Button)sender;
+                if (cells[count] == 0)
+                {
+                    button.Text = "*";
+                    MessageBox.Show("ТЫ ВЗОРВАН! НУ И НУ!");
+                    
+                }
+                else
+                {
+                    button.Text = "-";
+                    MessageBox.Show("СПАСИБО, ЧТО ЖИВОЙ");
+                }
+            
+        }
 
     }
 }
